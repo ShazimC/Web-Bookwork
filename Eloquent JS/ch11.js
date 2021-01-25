c07dd027fb3862b27b44d4c9813cc1c84ff5378a
Chaudhary, Shazim (sc276a) <sc276a@us.att.com>
2021-01-25 17:08:59 -0500

    ch11 async programming

--- Eloquent JS/ch11.js
+++ Eloquent JS/ch11.js
@@ -1,1 +1,35 @@
 // todo: Asynchronous Programming
+/* 
+    * The speed at which something like a loop executes depends on the
+    * PROCESSOR.
+*/
+
+/* 
+    * Asynchronity
+    - In a synchronous programming model, things happen one at a time.
+    - An asynchronous model allows multiple things to happen at the same time.
+    - Makes expressing programs that do not fit in the straight-line model
+        of control easier, but it can also make expressing programs that do
+        more awkward.
+*/
+
+/* 
+    * Crow Tech
+    - bruh
+*/
+
+/* 
+    * Callbacks
+    - One approach to async programming is to make functions that take
+    callback functions as an arg.
+    - So the func does something, then performs the callback func.
+*/
+
+// an example of a function that takes callbacks is
+setTimeout(() => {
+    console.log('this is the callback function work')
+}, 500);
+
+/* 
+    * Promises
+*/
