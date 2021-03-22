e903758025e64e6d4d17edf5987416a20dd85db0
Chaudhary, Shazim (sc276a) <sc276a@us.att.com>
2021-03-22 18:51:15 -0400

    ch11 upto async functions fr fr

--- Eloquent JS/ch11.js
+++ Eloquent JS/ch11.js
@@ -180,3 +180,38 @@ everywhere(nest => {
 	broadcastConnections(nest, nest.name);
 })
 
+function findRoute(from, to, connections){
+	let work = [{at: from, via: null}];
+	for(let i=0; i<work.length; i++){
+		let {at, via} = work[i];
+		for(let next of connections.get(at) || []){
+			if(next === to) return via;
+			if(!work.some(w => w.at === next)) {
+				work.push({at: next, via: via || next});
+			}
+		}
+	}
+	return null;
+}
+
+function routeRequest(nest, target, type, content){
+	if(nest.neighbors.includes(target)){
+		return request(nest, target, type, content);
+	} else {
+		let via = findRoute(nest.name, target, nest.state.connections);
+		if(!via) throw new Error(`No route to ${target}`);
+		return  request(nest, via, "route", {target, type, content});
+	}
+}
+
+requestType("route", (nest, {target, type, content}) => {
+	return routeRequest(nest, target, type, content);
+})
+
+/* 
+	* Async Functions
+	- 
+*/
+
+requestType("storage", (nest, name) => storage(nest, name));
+function findInStorage(nest, name)
