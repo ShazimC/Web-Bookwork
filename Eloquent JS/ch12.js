0934315b86e4e26e1054d411b38ef434245b5341
Shazim Chaudhary <gogito.3000@gmail.com>
2021-04-01 21:29:49 -0400

    Chapter 12

---
+++ Eloquent JS/ch12.js
@@ -0,0 +1,12 @@
+// todo: Chapter 12: Project - Programming Language
+
+/* 
+    * Parsing
+    - The most immediately visible part of a programming
+    language is the syntax.
+    * Parser - a program that reads a piece of text and produces
+    * a data structure that reflects the structure of the program
+    * contained in that text.
+        - if the text does not form a valid program, the parser
+        should point out the error.
+*/
