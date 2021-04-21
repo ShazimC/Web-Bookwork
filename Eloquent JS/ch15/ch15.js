5d19206fe1254b5dd74aae9a4b063e17a7a8a901
ShazimC <shc77@scarletmail.rutgers.edu>
2021-04-21 16:53:33 -0400

    ch15 upto propagation

---
+++ Eloquent JS/ch15/ch15.js
@@ -0,0 +1,13 @@
+//todo: Chapter 15 Handling Events
+/* 
+  * Event Handlers
+  - 
+*/
+
+/* 
+  * Events and DOM Nodes
+  - Each event handler is registered TO something.
+  - We can register event handlers to DOM nodes.
+  - Event listeners are called only when the event
+  happens in the context of the object they are registered on.
+*/
